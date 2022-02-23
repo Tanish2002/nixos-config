c96a035fe1b4589e92226087796975ef44ec2158
Tanish2002 <tanishkhare@gmail.com>
2022-02-23 22:31:54 +0530

    change firefoxcss to nix string

--- home/programs/firefox/css/userContent.nix
+++ home/programs/firefox/css/userContent.nix
@@ -1,25 +1,27 @@
-@import url("userChrome.css");
+''
+  @import url("${./userChrome.nix}");
 
-/* Removes white loading page */
-@-moz-document url(about:blank), url(about:newtab), url(about:home) {
-    html:not(#ublock0-epicker), html:not(#ublock0-epicker) body, #newtab-customize-overlay {
-      background: var(--mff-bg) !important;
+  /* Removes white loading page */
+  @-moz-document url(about:blank), url(about:newtab), url(about:home) {
+      html:not(#ublock0-epicker), html:not(#ublock0-epicker) body, #newtab-customize-overlay {
+        background: var(--mff-bg) !important;
+      }
     }
-  }
 
 
-  /* Hide scrollbar */
+    /* Hide scrollbar */
 
-  :root{
-    scrollbar-width: thin !important;
-    scrollbar-color: #5BA7D6 #0f0f13 !important;
-  }
+    :root{
+      scrollbar-width: thin !important;
+      scrollbar-color: #5BA7D6 #0f0f13 !important;
+    }
 
 
-  @-moz-document url(about:privatebrowsing) {
+    @-moz-document url(about:privatebrowsing) {
 
-  :root{
-    scrollbar-width: 1 !important;
-    scrollbar-color: #5BA7D6 !important;
-  }
-  }
+    :root{
+      scrollbar-width: 1 !important;
+      scrollbar-color: #5BA7D6 !important;
+    }
+    }
+''
