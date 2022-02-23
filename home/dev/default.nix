a98e468ab223d1e1b4237d363540263883cdce32
Tanish2002 <tanishkhare@gmail.com>
2022-02-24 01:11:33 +0530

    move ssh config to a seperate folder

--- home/dev/default.nix
+++ home/dev/default.nix
@@ -1,31 +1,10 @@
 { ... }: {
-  imports = [ ./vscode ];
+  imports = [ ./vscode ./ssh];
   programs = {
-    ssh = {
-      enable = true;
-      matchBlocks = {
-        #using github.com so it's default and easier to just copy paste
-        "github.com" = {
-          hostname = "github.com";
-          user = "git";
-          identityFile = "${./keys/personal/key}";
-          identitiesOnly = true;
-        };
-        "work" = {
-          hostname = "github.com";
-          user = "git";
-          identityFile = "${./keys/work/key}";
-          identitiesOnly = true;
-        };
-      };
-    };
     git = {
       enable = true;
       userName = "Tanish2002";
       userEmail = "tanishkhare@gmail.com";
-    };
-    gpg = {
-      enable = true;
     };
   };
 }
---
+++ home/dev/ssh/default.nix
@@ -0,0 +1,20 @@
+{ ... }: {
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      #using github.com so it's default and easier to just copy paste
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "${./keys/personal/key}";
+        identitiesOnly = true;
+      };
+      "work" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "${./keys/work/key}";
+        identitiesOnly = true;
+      };
+    };
+  };
+}
