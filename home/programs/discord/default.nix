78cfb0aa4b1fef2e0f9d638ba8e738ae6db3956d
Tanish2002 <tanishkhare@gmail.com>
2023-08-03 02:27:14 +0530

    use discord with openasar over discocss

--- home/programs/discord/default.nix
+++ home/programs/discord/default.nix
@@ -1,8 +1,23 @@
-{pkgs, ...}: {
-  programs.discocss = {
-    enable = true;
-    discordPackage = pkgs.discord;
-    discordAlias = true;
-    css = import ./discordcss.nix;
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  settingsJson = {
+    openasar = {
+      css = import ./discordcss.nix;
+    };
   };
+in {
+  home.packages = [(pkgs.discord.override {withOpenASAR = true;})];
+  home.activation.install-discord-theme = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    $DRY_RUN_CMD mkdir -p "${config.xdg.configHome}/discord"
+    if [ -e ${config.xdg.configHome}/discord/settings.json ]; then
+      $DRY_RUN_CMD ${pkgs.jq}/bin/jq '.openasar.css = "${import ./discordcss.nix}"' ${config.xdg.configHome}/discord/settings.json > ${config.xdg.configHome}/discord/temp.json && $DRY_RUN_CMD mv ${config.xdg.configHome}/discord/temp.json ${config.xdg.configHome}/discord/settings.json
+    else
+      $DRY_RUN_CMD touch "${config.xdg.configHome}/discord/settings.json"
+      $DRY_RUN_CMD echo '${builtins.toJSON settingsJson}' > ${config.xdg.configHome}/discord/settings.json
+    fi
+  '';
 }
